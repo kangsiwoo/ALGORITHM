bee634fc9da979e903655db18f413060943ad88f
kangsiwoo <2023003@bssm.hs.kr>
2023-05-18 04:37:28 +0900

    codeup : to teacher

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include "120820.c"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[])
+{
+    printf("nayoung12선생님 감사합니다.\n");
+    printf("아직 개학한지 얼마 지나지 않았지만 저를 잘 봐주시고 항상 응원해주실것을 믿고\n열심히 공부하겠습니다. 그리고 선생님도 제가 기억에 남았으면 좋겠습니다.\n그럼 짧은 편지는 여기서 끝내도록 하겠습니다. 결혼 축하드립니다.");
     return 0;
 }
